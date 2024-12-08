d2809ffc6054a3db9f275c468468aef0d533a421
balaji <balaji.kukati111@gmail.com>
2024-12-08 06:56:52 +0530

    update two

--- sample.sh
+++ sample.sh
@@ -14,7 +14,4 @@ echo "Hello, World!"
 echo "Welocome IDA"
 echo "balaji"
 echo "yaswanth"
-echo "subbamma"
-echo "aws devops"
->>>>>>> 100d07ed956e8b5f9fe19aa5b1231baf64616b23
->>>>>>> 49148a6f584dbe86cfb0db85cebce043ea15d138
+welcome to legacie techno
